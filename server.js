b7cd779203fa2e2b6eeb35fda0f3248f233b47a3
Vaibhav Arora <varora@catalystone.com>
2019-07-11 14:24:07 +0530

    -- 1 more attempty with directory for server.js to get deployed

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/bank-search-screen'));
+app.use(express.static(__dirname + '/dist/bank-search-screen'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname,'/dist/bank-search-screen/index.html'));
 });
 
